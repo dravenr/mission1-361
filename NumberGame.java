b08dd04cf350a7cd4eea67c78ab6026d5b08ea62
Draven Ramirez <89436111+dravenr@users.noreply.github.com>
2022-09-01 11:29:04 -0500

    Add files via upload

---
+++ NumberGame.java
@@ -0,0 +1,28 @@
+package mini2;
+
+import java.util.ArrayList;
+
+/**
+ * Implementation of a version of the "twenty-four" game.
+ */
+public class NumberGame {
+
+	/**
+	 * Lists all ways to obtain the given target number using arithmetic operations
+	 * on the values in the given IntExpression list. Results in string form are
+	 * added to the given list, where the string form of a value is obtained from
+	 * the toString() of the Value object. Special rules are: 1) you are not
+	 * required to use all given numbers, and 2) division is integer division and is
+	 * only allowed when remainder is zero. For addition and multiplication, a + b
+	 * and b + a are considered to be distinct solutions, and likewise a * b and b *
+	 * a are considered as different solutions. See the pdf for detailed examples.
+	 * 
+	 * @param list    the values to be used
+	 * @param target  the target number to be obtained from the values in the list
+	 * @param results list of all results, as strings
+	 */
+	public static void findSolution(ArrayList<IntExpression> list, int target, ArrayList<String> results) {
+		// TODO
+	}
+
+}
